745f62f26b3ad9c18a59ae50306cae6a5041cbd1
Antonio Roldan Andrade <antonioroldanandrade@gmail.com>
2021-03-24 19:32:20 +0100

    pibes

---
+++ cmake-build-debug/CMakeFiles/EDNLv2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EDNLv2.dir/main.cpp.obj"
+  "EDNLv2.exe"
+  "EDNLv2.exe.manifest"
+  "EDNLv2.pdb"
+  "libEDNLv2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EDNLv2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
