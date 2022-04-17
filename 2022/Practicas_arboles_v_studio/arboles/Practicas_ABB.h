1a1f621b78295b406af590525c929799b1c14115
mry3ll0w <aradxc56@gmail.com>
2022-04-17 11:14:21 +0200

    Subida para torre

--- 2022/Practicas_arboles_v_studio/arboles/Practicas_ABB.h
+++ 2022/Practicas_arboles_v_studio/arboles/Practicas_ABB.h
@@ -1,2 +1,58 @@
 #include "abb.h"
+#include <ostream>
+#include <vector>
+#include <list>
 
+//Impresion del arbol
+template<class t>
+void imprime_arbol (const Abb<t>& a) {
+	if (!a.vacio())
+	{
+		std::cout << a.elemento() << ", ";
+
+		imprime_arbol(a.izqdo());
+		imprime_arbol(a.drcho());
+
+	}
+	
+}
+
+
+//EJERCICIO 1
+
+
+template<class t>
+void podar_abb(const t& buscado, Abb<t>&A) {
+
+	//Si esta vacio no hacemos nada, ya que o no lo encuentras o esta vacio
+	Abb<t> arbol_temp = A.buscar(buscado);
+	if (!arbol_temp.vacio())
+	{
+
+		A.buscar(buscado).~Abb();
+
+	}
+
+}
+
+/*
+Un árbol binario de búsqueda se puede equilibrar realizando el recorrido en inorden
+del árbol para obtener el listado ordenado de sus elementos y a continuación, repartir
+equitativamente los elementos a izquierda y derecha colocando la mediana en la raíz y
+construyendo recursivamente los subárboles izquierdo y derecho de cada nodo.
+Implementa este algoritmo para equilibrar un ABB.
+*/
+
+//Obtiene la lista de elementos de un arbol binario
+template<class t>
+std::list<t> elementos_abb(Abb<t> A,std::list<t>& l) {
+
+	if (!A.vacio())
+	{
+		elementos_abb(A.izqdo(), l);
+		l.push_back(A.elemento());
+		elementos_abb(A.drcho(), l);
+	}
+
+	return l;
+}
--- 2022/Practicas_arboles_v_studio/arboles/arboles.cpp
+++ 2022/Practicas_arboles_v_studio/arboles/arboles.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include "abb.h"
 #include "Apo.h"
+#include "Practicas_ABB.h"
 int main()
 {
 
     Abb<int>A;
-    A.insertar(6);
+    A.insertar(7);
+    A.insertar(5);
+    A.insertar(12);
+    A.insertar(9);
+    A.insertar(17);
     A.insertar(8);
-    A.insertar(1);
+    A.insertar(11);
+    A.insertar(10);
 
-    std::cout << A.elemento() << std::endl;
+    std::list<int> l;
+    l=elementos_abb(A, l);
+    l.sort();
+    for (auto i : l) {
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
+    imprime_arbol(A);
+    
+    return 0;
 }
 
