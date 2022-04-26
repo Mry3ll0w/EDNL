e66016fea37e1d6ff657a96b32df06a9a7c01bca
MrY3ll0w <aradxc56@gmail.com>
2022-04-26 15:46:45 +0200

    :sparkles:

--- 2022/arboles/examen_infimo_sup.cpp
+++ 2022/arboles/examen_infimo_sup.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include "abb.h"
 
 template<class t>
@@ -45,4 +46,71 @@ t minimo_rec(Abb<t>A){
     }
 
     return minimo_rec(A.izqdo());
+}
+
+//se define el inifimo de un numero como el numero <= x
+//ej 4 5 6 12 23, si x = 6 el infimo es el mayor de los menores, inf = 5
+template<class t>
+void infimo_rec(Abb<t>A, t& infimo, const t x){
+
+    if(!A.vacio()){
+        
+        if (A.elemento() == x )
+        {
+            infimo=x;
+        }
+        else if(A.elemento() < x){
+            
+            if(infimo < A.elemento()){
+                infimo = A.elemento();
+            }
+
+        }
+        
+        infimo_rec(A.izqdo(),infimo,x);   
+        infimo_rec(A.drcho(),infimo,x);   
+    }
+
+}
+
+template<class t>
+void supremo_rec(Abb<t>A,const t x,  t& supremo){
+    if(!A.vacio()){
+        
+        if (A.elemento() == x )
+        {
+            supremo=x;
+        }
+        else if(A.elemento() > x){
+            
+            if(supremo > A.elemento()){
+                supremo = A.elemento();
+            }
+
+        }
+        
+        infimo_rec(A.izqdo(),supremo,x);   
+        infimo_rec(A.drcho(),supremo,x);   
+    }
+}
+
+template <class t>
+void infimo_supremo(Abb<t> &A, const t& x){
+
+    t infimo=std::numeric_limits<t>::max();
+    t supremo= std::numeric_limits<t>::min();
+
+    infimo_rec(A,x);
+    supremo_rec(A,x);
+
+    //si supremo/minimo no han variado su valor devuelve el maximo/minimo del arbol
+    if (infimo == std::numeric_limits<t>::max())
+    {
+        infimo = minimo_rec(A),
+    }
+    else if(supremo == std::numeric_limits<t>::min()){
+        supremo = maximo_rec(A);
+    }
+
+    std::cout<<"El infimo: "<<infimo<<"\t Supremo: "<<supremo<<std::endl;    
 }
