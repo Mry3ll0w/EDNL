657b3c0cf023aad0138516e155595f33bc8f798e
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2023-07-25 14:58:03 +0200

    Desequilibrio

--- 2023/arboles/verano.cpp
+++ 2023/arboles/verano.cpp
@@ -33,6 +33,52 @@ int alturaArbolBinario(Abin<T> &abArbol, typename Abin<T>::nodo nd)
                 alturaArbolBinario(abArbol, abArbol.hijoIzqdo(nd)));
 }
 
+/**
+ * Ejercicio 3 :
+ * Implementa un subprograma que, dados un árbol binario y un nodo del mismo, determine la
+ * profundidad de este nodo en dicho árbol.
+ */
+template <class T>
+int profundidadNodoAbin(Abin<T> &abArbol, typename Abin<T>::nodo nd)
+{
+    if (nd == Abin<T>::NODO_NULO)
+        return 0;
+    return 1 + profundidadNodoAbin(abArbol, abArbol.padre(nd));
+}
+
+/**
+ * Ejercicio 6
+ * Implementa un subprograma que determine el nivel de desequilibrio de un árbol binario,
+ * definido como el máximo desequilibrio de todos sus nodos. El desequilibrio de un nodo
+ * se define como la diferencia entre las alturas de los subárboles del mismo.
+ */
+
+template <class T>
+int diferenciaAlturas(Abin<T> abArbol, typename Abin<T>::nodo nd)
+{
+    return std::abs(alturaArbolBinario(abArbol, abArbol.hijoDrcho(nd)) - alturaArbolBinario(abArbol, abArbol.hijoIzqdo(nd)));
+}
+
+template <class T>
+int desequilibrioMaximoRec(Abin<T> abArbol, typename Abin<T>::nodo nd1)
+{
+    if (nd == Abin<T>::NODO_NULO)
+    {
+        return 0;
+    }
+    return std::fmax(desequilibrioMaximoRec(abArbol, nd),
+                     desequilibrioMaximoRec(abArbol, abArbol.hijoDrcho(nd)), abArbol.hijoIzqdo(nd));
+}
+
+template <class T>
+int desequilibrioAbin(Abin<T> abArbol)
+{
+    if (abArbol.arbolVacio())
+        return 0;
+    else
+        return desequilibrioMaximoRec(abArbol, abArbol.raiz());
+}
+
 int main()
 {
     Abin<int> ab;
